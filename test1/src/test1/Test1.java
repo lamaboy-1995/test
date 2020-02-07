e448aa0ef1ff23acca4e24b79127e07a990cdb7f
Libh <Libh@DESKTOP-7DP0G1G.mshome.net>
2020-02-07 16:18:04 +0800

    同时提交本地和Git

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub!");
 		System.out.println("Hello 测试!");
+		System.out.println("哥呢鸡哇");
 	}
 
 }
--- test1/src/test1/Test2.java
+++ test1/src/test1/Test2.java
@@ -1,2 +1,10 @@
+package test1;
+
 // 1213131321
 // abacacacba
+class a1{
+	private void ect() {
+		// TODO Auto-generated method stub
+
+	}
+}
